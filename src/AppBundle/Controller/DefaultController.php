6d37233cb67f5526e780f577a0dd8b81e1886765
Nate Turner <nturner@ballytech.com>
2015-01-07 16:18:13 -0800

    add endpoint to set access token

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use OAuth\OAuth1\Token\StdOAuth1Token;
 use OAuth\ServiceFactory;
 use OAuth\Common\Storage\Session;
 use AppBundle\OAuth\WithingsOAuth;
@@ -63,6 +64,22 @@ class DefaultController extends Controller
         $uri = 'measure?action=getmeas&userid=5702500';
 
         print_r($withingsAdapter->getWithingsService()->request($uri));
+        exit;
+    }
+
+    /**
+     * @Route("/withings/store_token")
+     */
+    public function withingsStoreToken()
+    {
+        /** @var WithingsApiAdapter $withingsAdapter */
+        $withingsAdapter = $this->get('withings_api_adapter');
+
+        $token = new StdOAuth1Token();
+        $token->setAccessToken('d3c14c35646714efec8ce1552484615c7453e5212064d82fc87f349f');
+        $token->setAccessTokenSecret('0048ee0d58343d704c21be647587ee63e011474e4c8de4386e7d14e7822');
+
+        $withingsAdapter->getWithingsService()->getStorage()->storeAccessToken('WithingsOAuth', $token);
         exit;
     }
 
