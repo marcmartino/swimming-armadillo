874849fc9abdc563ca032c11282c6d2f07f255fd
Nate Turner <nater1992@gmail.com>
2015-04-26 17:30:05 -0700

    start using api parser classes

---
+++ src/AppBundle/Tests/ApiParser/FitbitWeightTest.php
@@ -0,0 +1,37 @@
+<?php
+use AppBundle\ApiParser\FitbitWeight;
+use AppBundle\Entity\Measurement;
+use AppBundle\Entity\MeasurementEvent;
+
+/**
+ * Created by PhpStorm.
+ * User: Nate
+ * Date: 4/26/15
+ * Time: 5:04 PM
+ */
+
+class FitbitWeightTest extends PHPUnit_Framework_TestCase {
+    public function testParse()
+    {
+        $entityManager = $this
+            ->getMockBuilder('\Doctrine\ORM\EntityManager')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $responseBody = file_get_contents(__DIR__ . '/../Resources/ApiParser/fitbitWeight.json');
+        $parser = new FitbitWeight($entityManager);
+        $results = $parser->parse($responseBody);
+
+        $this->assertCount(2, $results['measurements']);
+        $this->assertCount(2, $results['measurement_events']);
+
+        /** @var MeasurementEvent $measurementEvent */
+        $measurementEvent = $results['measurement_events'][0];
+        $expectedEventDateTime = new DateTime('2012-03-05 23:59:59');
+        $this->assertEquals($expectedEventDateTime->getTimestamp(), $measurementEvent->getEventTime()->getTimestamp());
+
+        /** @var Measurement $measurement */
+        $measurement = $results['measurements'][1];
+        $this->assertEquals(72500, $measurement->getUnits());
+    }
+}
