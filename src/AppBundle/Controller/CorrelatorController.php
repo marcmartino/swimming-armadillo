5f8f584fc5521b4ad70c2c04a880072fe8e0f5cd
Nathan Turner <nturner@ballytech.com>
2015-05-12 13:58:56 -0700

    use pearson correlation

--- src/AppBundle/Controller/CorrelatorController.php
+++ src/AppBundle/Controller/CorrelatorController.php
@@ -18,7 +18,7 @@ class CorrelatorController extends Controller {
     public function indexAction(Request $request)
     {
         /** @var SimpleSlope $correlator */
-        $correlator = $this->get('correlator.simple_slope');
+        $correlator = $this->get('correlator.pearson');
 
         $start = new DateTime('2015-02-15');
         $end = new DateTime('2015-04-01');
@@ -29,41 +29,23 @@ class CorrelatorController extends Controller {
         $measurementType = $this->getDoctrine()->getEntityManager()->getRepository('AppBundle:MeasurementType')
             ->findOneBy(['slug' => MeasurementType::WEIGHT]);
         $weightUserData = $userData->getUserData($measurementType->getId(), $start, $end);
-
-        $weightData = [
-            [
-                'timestamp' => (new DateTime($weightUserData[0]['event_time']))->getTimestamp(),
-                'units' => $weightUserData[0]['units']
-            ],
-            [
-                'timestamp' => (new DateTime(end($weightUserData)['event_time']))->getTimestamp(),
-                'units' => end($weightUserData)['units']
-            ]
-        ];
+        array_walk($weightUserData, function (&$item, $i) {
+            $item['timestamp'] = new DateTime($item['event_time']);
+        });
 
         $measurementType = $this->getDoctrine()->getEntityManager()->getRepository('AppBundle:MeasurementType')
             ->findOneBy(['slug' => MeasurementType::FAT_RATIO]);
         $bodyfatUserData = $userData->getUserData($measurementType->getId(), $start, $end);
-
-        $bodyFatData = [
-            [
-                'timestamp' => (new DateTime($bodyfatUserData[0]['event_time']))->getTimestamp(),
-                'units' => $bodyfatUserData[0]['units']
-            ],
-            [
-                'timestamp' => (new DateTime(end($bodyfatUserData)['event_time']))->getTimestamp(),
-                'units' => end($bodyfatUserData)['units']
-            ]
-        ];
+        array_walk($bodyfatUserData, function (&$item, $i) {
+            $item['timestamp'] = new DateTime($item['event_time']);
+        });
 
         return $this->render('correlator/index.html.twig', [
             'start' => $start,
             'end' => $end,
-            'weight_data' => $weightData,
-            'weight_slope' => $correlator->calculateSlopOfDataSet($weightData),
-            'bodyfat_data' => $bodyFatData,
-            'bodyfat_slope' => $correlator->calculateSlopOfDataSet($bodyFatData),
-            'correlation' => $correlator->getCorrelation($weightData, $bodyFatData)
+            'weight_data' => $weightUserData,
+            'bodyfat_data' => $bodyfatUserData,
+            'correlation' => $correlator->getCorrelation($weightUserData, $bodyfatUserData)
         ]);
     }
 }
