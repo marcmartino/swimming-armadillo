8a76a02acdc83f4a6dc5e9bee461b88400b41fc4
Nathan Turner <nturner@ballytech.com>
2015-05-23 13:28:18 -0700

    start homepage

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -12,19 +12,19 @@ class DefaultController extends Controller
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction()
-    {
-        if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
-            /** @var OAuthAccessToken $oauthAccessTokenEntity */
-            $oauthAccessTokenEntity = $this->get('entity.oauth_access_token');
-            $oauthTokens = $oauthAccessTokenEntity->getUserOAuthAccessTokens($this->getUser()->getId());
-            if (count($oauthTokens) === 0) {
-                // If the user has not already set up their service providers
-                return $this->forward('AppBundle:Provider:providers');
-            }
-            // Otherwise forward them to the graph page
-            return $this->forward('AppBundle:Graph:graph');
-        }
-        return $this->render('default/index.html.twig');
-    }
+//    public function indexAction()
+//    {
+//        if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+//            /** @var OAuthAccessToken $oauthAccessTokenEntity */
+//            $oauthAccessTokenEntity = $this->get('entity.oauth_access_token');
+//            $oauthTokens = $oauthAccessTokenEntity->getUserOAuthAccessTokens($this->getUser()->getId());
+//            if (count($oauthTokens) === 0) {
+//                // If the user has not already set up their service providers
+//                return $this->forward('AppBundle:Provider:providers');
+//            }
+//            // Otherwise forward them to the graph page
+//            return $this->forward('AppBundle:Graph:graph');
+//        }
+//        return $this->render('default/index.html.twig');
+//    }
 }
