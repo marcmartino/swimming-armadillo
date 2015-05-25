2ab5ad06f07baa53398769cd39e0b86e0bdd5cc1
Nathan Turner <nturner@ballytech.com>
2015-05-25 16:46:57 -0700

    more error handling fitbit

--- src/AppBundle/ApiParser/Fitbit/AbstractFitbitApiParser.php
+++ src/AppBundle/ApiParser/Fitbit/AbstractFitbitApiParser.php
@@ -12,11 +12,16 @@ class AbstractFitbitApiParser extends AbstractEntityApiParser
 {
     /**
      * Check response and parse any errors
+     * @param $responseBody
+     * @throws ApiParserException
      */
     public function parseError($responseBody)
     {
         $json = json_decode($responseBody, true);
-        if ($json['success'] == false) {
+        if (isset($json['success']) && $json['success'] == false) {
+            throw new ApiParserException('Fibit api request failed: ' . $responseBody);
+        }
+        if (isset($json['errors'])) {
             throw new ApiParserException('Fibit api request failed: ' . $responseBody);
         }
     }
