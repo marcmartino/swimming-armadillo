dbb1a57b294922d67bd38891b866c56fcc0f7812
Nate Turner <nater1992@gmail.com>
2015-05-30 09:52:28 -0700

    subscribe users to mailchimp when registering

--- src/AppBundle/EventListener/FOSUserBundleRegistrationInitialized.php
+++ src/AppBundle/EventListener/FOSUserBundleRegistrationInitialized.php
@@ -3,6 +3,8 @@ namespace AppBundle\EventListener;
 
 use Doctrine\ORM\EntityManagerInterface;
 use FOS\UserBundle\Event\UserEvent;
+use Mailchimp;
+use Mailchimp_Error;
 
 /**
  * Class FOSUserBundleRegistrationInitialized
@@ -12,14 +14,24 @@ class FOSUserBundleRegistrationInitialized {
 
     /** @var EntityManagerInterface */
     protected $em;
+    /** @var Mailchimp */
+    protected $mailchimp;
+    /** @var string - the id of the user mailing list with Mailchimp */
+    protected $mailingListId;
 
     /**
      * @param EntityManagerInterface $em
+     * @param Mailchimp $mailchimp
+     * @param $mailingListId
      */
     public function __construct(
-        EntityManagerInterface $em
+        EntityManagerInterface $em,
+        Mailchimp $mailchimp,
+        $mailingListId
     ) {
         $this->em = $em;
+        $this->mailchimp = $mailchimp;
+        $this->mailingListId = $mailingListId;
     }
 
     /**
@@ -28,13 +40,34 @@ class FOSUserBundleRegistrationInitialized {
     public function processEvent(UserEvent $event)
     {
         $request = $event->getRequest();
+        /** @var \AppBundle\Entity\User $user */
+        $user = $event->getUser();
         $formFields = $request->get('fos_user_registration_form');
-        $registrationCode = $this->em->getRepository('AppBundle:RegistrationCode')
-            ->findOneBy(['code' => $formFields['registrationCodeCode']]);
-        if (!empty($registrationCode)) {
-            $event->getUser()->setRegistrationCode($registrationCode);
-        } else {
-            // TODO Handle lock
+        // If the form has been submitted
+        if (!empty($formFields)) {
+            $registrationCode = $this->em->getRepository('AppBundle:RegistrationCode')
+                ->findOneBy(['code' => $formFields['registrationCodeCode']]);
+            if (!empty($registrationCode)) {
+                $user->setRegistrationCode($registrationCode);
+            } else {
+                // Disable accounts without a valid code
+                $user->setEnabled(false);
+            }
+            $this->addUserToMailchimp($formFields['email']);
+        }
+    }
+
+    /**
+     * Add a user's email to our mailing list
+     * @param $email - the user's email to add
+     */
+    public function addUserToMailchimp($email)
+    {
+        try {
+            $this->mailchimp->lists->subscribe($this->mailingListId, ['email' => $email]);
+        } catch (Mailchimp_Error $e) {
+            // TODO handle subscription errors
+            error_log($e->getMessage());
         }
     }
 }
