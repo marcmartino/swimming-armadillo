d0853864a2d70e30b5a84fa037e3bbd68c569b16
Nate Turner <nater1992@gmail.com>
2015-06-06 10:15:42 -0700

    forward controller appropriately

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,7 +14,7 @@ class DefaultController extends Controller
     public function indexAction()
     {
         if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
-            return $this->forward('AppBundle:Provider:providers');
+            return $this->forward('app.controller.provider_controller:providersAction');
         }
         return $this->render('default/index.html.twig');
     }
