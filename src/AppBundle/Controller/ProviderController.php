5150e8af29b5444e9b885c64142059f8ff516ec6
Nate Turner <nater1992@gmail.com>
2015-06-06 08:51:21 -0700

    green for authenticated service providers

--- src/AppBundle/Controller/ProviderController.php
+++ src/AppBundle/Controller/ProviderController.php
@@ -1,27 +1,75 @@
 <?php
 namespace AppBundle\Controller;
+
 use AppBundle\ApiAdapter\ProviderApiAdapterFactory;
+use AppBundle\Entity\ServiceProviderRepository;
 use AppBundle\Exception\UserNotAuthenticatedWithServiceProvider;
 use OAuth\Common\Exception\Exception as OAuthException;
+use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\Security\Core\SecurityContextInterface;
+
 /**
  * Class ProviderController
  * @package AppBundle\Controller
+ * @Route(service="app.controller.provider_controller")
  */
 class ProviderController extends Controller
 {
+    /**
+     * @var ServiceProviderRepository
+     */
+    protected $serviceProviders;
+    /**
+     * @var SecurityContextInterface
+     */
+    private $securityContext;
+    /**
+     * @var EngineInterface
+     */
+    private $templating;
+
+    /**
+     * @param ServiceProviderRepository $serviceProviders
+     * @param SecurityContextInterface $securityContext
+     * @param EngineInterface $templating
+     * @param ContainerInterface $container // TODO remove this
+     */
+    public function __construct(
+        ServiceProviderRepository $serviceProviders,
+        SecurityContextInterface $securityContext,
+        EngineInterface $templating,
+        ContainerInterface $container
+    ) {
+        $this->serviceProviders = $serviceProviders;
+        $this->securityContext = $securityContext;
+        $this->templating = $templating;
+        $this->container = $container;
+    }
+
     /**
      * @Route("/providers", name="providers")
      */
     public function providersAction()
     {
-        $providers = $this->getDoctrine()->getRepository('AppBundle:ServiceProvider')
+        $providers = $this->serviceProviders
             ->findAll();
-        return $this->render("provider/providers.html.twig", [
-            'providers' => $providers
+        /** @var \AppBundle\Entity\User $user */
+        $user = $this->securityContext->getToken()->getUser();
+        $oauthAccessTokens = $user->getOauthAccessTokens();
+        $authenticatedProviders = [];
+        foreach ($oauthAccessTokens as $token) {
+            $authenticatedProviders[] = $token->getServiceProvider()->getId();
+        }
+        print_r($authenticatedProviders);
+        return $this->templating->renderResponse("provider/providers.html.twig", [
+            'providers' => $providers,
+            'authenticatedProviders' => $authenticatedProviders
         ]);
     }
     /**
